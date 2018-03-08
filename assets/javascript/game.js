0be7f75482566009fb0ca646041abb06eeeeebc2
unknown <cole.hansen@gmail.com>
2018-03-08 11:44:46 -0800

    Add fucntion to fill in correct guesses to replace spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ var wordOptions = [
 var spaces5 = ["_ ", "_ ", "_ ", "_ ", "_ "];
 var spaces6 = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ "];
 var spaces7 = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "];
-var lettersGuessed = [];
+var wrongGuesses = [];
 var wins = 0;
 var remainingGuesses = 12;
 var wordToGuess = wordOptions[Math.floor(Math.random()*wordOptions.length)];
@@ -23,28 +23,60 @@ document.onkeypress = function (event) {
 
     if (playerGuess == "Enter") {
         wordToGuess = wordOptions[Math.floor(Math.random()*wordOptions.length)];
+        wrongGuesses = [];
     };
 
     if (wordToGuess.length == 5) {
-        document.getElementById("spaces").innerHTML = spaces5;
+        spaces = spaces5
+        document.getElementById("spaces").innerHTML = spaces;
     } else if (wordToGuess.length == 6) {
-        document.getElementById("spaces").innerHTML = spaces6;
+        spaces = spaces6
+        document.getElementById("spaces").innerHTML = spaces;
     } else if (wordToGuess.length == 7) {
-        document.getElementById("spaces").innerHTML = spaces7;
+        spaces = spaces7
+        document.getElementById("spaces").innerHTML = spaces;
     };
 
-    if (playerGuess.toLowerCase() != playerGuess.toUpperCase() && lettersGuessed.indexOf(event.key) == -1 && playerGuess != "Enter" && playerGuess != "Space") {
+    if (playerGuess.toLowerCase() != playerGuess.toUpperCase() && wrongGuesses.indexOf(event.key) == -1 && playerGuess != "Enter" && playerGuess != "Space") {
+        if (playerGuess == wordToGuess.charAt(0)) {
+            spaces.splice(0, 1, playerGuess);
+            print();
+        } else if (playerGuess == wordToGuess.charAt(1)) {
+            spaces.splice(1, 1, playerGuess);
+            print();
+        } else if (playerGuess == wordToGuess.charAt(2)) {
+            spaces.splice(2, 1, playerGuess);
+            print();
+        } else if (playerGuess == wordToGuess.charAt(3)) {
+            spaces.splice(3, 1, playerGuess);
+            print();
+        } else if (playerGuess == wordToGuess.charAt(4)) {
+            spaces.splice(4, 1, playerGuess);
+            print();
+        } else if (playerGuess == wordToGuess.charAt(5)) {
+            spaces.splice(5, 1, playerGuess);
+            print();
+        } else if (playerGuess == wordToGuess.charAt(6)) {
+            spaces.splice(6, 1, playerGuess);
+            print();
+        }
         
-        lettersGuessed.push(playerGuess)
-        document.getElementById("letters-guessed").innerHTML = " " + lettersGuessed;
+        else {
+        wrongGuesses.push(playerGuess)
+        document.getElementById("letters-guessed").innerHTML = " " + wrongGuesses;
+        console.log(wrongGuesses);
+        remainingGuesses = remainingGuesses--;
         print();
-        console.log(lettersGuessed);
+        };
     };
 
 };
 
 function print() {
-console.log(lettersGuessed.length);
+document.getElementById("spaces").innerHTML = spaces;
+document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
+console.log(spaces);
+console.log(wrongGuesses.length);
 console.log(wins);
 console.log(remainingGuesses);
 console.log(wordToGuess);
